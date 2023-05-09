ea684ca0436ca737633fa8ec783bd399f2ae01c2
Patrik Lindfors <patrikwlindfors@gmail.com>
2023-05-09 17:19:21 +0200

    on tile index set event mm

--- SudokuProject/Assets/Scripts/EventManager.cs
+++ SudokuProject/Assets/Scripts/EventManager.cs
@@ -7,9 +7,15 @@ using UnityEngine.Events;
 public static class EventManager
 {
     public static UnityAction<SudokuGrid9x9> OnGridGenerated;
+    public static UnityAction<int,int, TileBehaviour> OnTileIndexSet;
 
     public static void GenerateGrid(SudokuGrid9x9 grid)
     {
         OnGridGenerated?.Invoke(grid);
+    }
+
+    public static void SetTileIndex(int row, int col, TileBehaviour tileBehaviour)
+    {
+        OnTileIndexSet?.Invoke(row, col, tileBehaviour);
     }
 }
--- SudokuProject/Assets/Scripts/Sudoku/GridBehaviour.cs
+++ SudokuProject/Assets/Scripts/Sudoku/GridBehaviour.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 
 public class GridBehaviour : MonoBehaviour
 {
+    private SudokuGrid9x9 grid;
+    private TileBehaviour[,] tileBehaviours = new TileBehaviour[9,9];
+    
     private void OnEnable()
     {
         EventManager.OnGridGenerated += OnGridGenerated;
+        EventManager.OnTileIndexSet += OnTileIndexSet;
     }
     
     private void OnDisable()
     {
         EventManager.OnGridGenerated -= OnGridGenerated;
+        EventManager.OnTileIndexSet -= OnTileIndexSet;
     }
 
-    private void OnGridGenerated(SudokuGrid9x9 arg0)
+    private void OnGridGenerated(SudokuGrid9x9 generatedGrid)
     {
-        throw new NotImplementedException();
+        grid = generatedGrid;
+    }
+    
+    private void OnTileIndexSet(int row,  int col, TileBehaviour tileBehaviour)
+    {
+        tileBehaviours[row, col] = tileBehaviour;
     }
 }
