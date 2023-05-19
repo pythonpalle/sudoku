38fe632f55c37c840341ff4efcba8f703fd578d1
pythonpalle <patrikwlindfors@gmail.com>
2023-05-19 08:27:35 +0200

    skapat underlag för hidden metoder

---
+++ SudokuProject/Assets/Scripts/Sudoku/SolveMethods/Candidate Methods/Hidden Multiple/Hidden Triple Methods/HiddenTripleInCol.cs
@@ -0,0 +1,4 @@
+﻿public class HiddenTripleInCol : HiddenMultiple
+{
+    
+}
--- SudokuProject/Assets/Scripts/Sudoku/SolveMethods/Candidate Methods/Hidden Multiple/HiddenMultiple.cs
+++ SudokuProject/Assets/Scripts/Sudoku/SolveMethods/Candidate Methods/Hidden Multiple/HiddenMultiple.cs
@@ -3,18 +3,3 @@ public abstract class HiddenMultiple : CandidateMethod
 {
     
 }
-
-public class HiddenPair : HiddenMultiple
-{
-    
-}
-
-public class HiddenTriple : HiddenMultiple
-{
-    
-}
-
-public class HiddenQuad : HiddenMultiple
-{
-    
-}
