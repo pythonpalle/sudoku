a9eabf373ea231506d5aab10e75d80a7d5c69552
Patrik Lindfors <patrikwlindfors@gmail.com>
2023-05-19 14:58:10 +0200

    Update FishMethod.cs

--- SudokuProject/Assets/Scripts/Sudoku/SolveMethods/Candidate Methods/Fish Methods/FishMethod.cs
+++ SudokuProject/Assets/Scripts/Sudoku/SolveMethods/Candidate Methods/Fish Methods/FishMethod.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FishMethod : MonoBehaviour
+public class FishMethod : CandidateMethod
 {
-    // Start is called before the first frame update
-    void Start()
+    protected bool TryFindFishInRow(SudokuGrid9x9 grid, int multCount, out CandidateRemoval removal)
     {
-        
+        return CandidatesFromFishInRowCol(grid, multCount, true, out removal);
+    }
+    
+    protected bool TryFindFishInCol(SudokuGrid9x9 grid, int multCount, out CandidateRemoval removal)
+    {
+        return CandidatesFromFishInRowCol(grid, multCount, false, out removal);
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool CandidatesFromFishInRowCol(SudokuGrid9x9 grid, int multCount, bool fishInRow, out CandidateRemoval removal)
     {
+        /*
+         
+         diciionary candCount = int (siffra) : int (antal av siffran i kolumnen)
+        för varje rad:
+            för varje siffra:
+                för varje kolumn:
+                    tile = rad, col
+                    om tile har kandidat siffra:
+                        candCount[siffra]++;
         
+        
+        */
+        throw new System.NotImplementedException();
     }
 }
