96c6bb0763d9ad8cc830ef9fb50ed3e50af563b9
pythonpalle <patrikwlindfors@gmail.com>
2023-11-29 11:54:56 +0100

    navigera till create own

--- SudokuProject/Assets/GameSceneManager.cs
+++ SudokuProject/Assets/GameSceneManager.cs
@@ -8,6 +8,7 @@ public class GameSceneManager : MonoBehaviour
     private static string startSceneName = "Scenes/Start Scene";
     private static string puzzleSceneName = "Scenes/Puzzle Select Scene";
     private static string gameSceneName = "Scenes/Game Scene";
+    private static string createOwnSceneName = "Scenes/Create Own Scene";
 
     public static void LoadStartScene()
     {
@@ -22,5 +23,10 @@ public class GameSceneManager : MonoBehaviour
     public static void LoadGameScene()
     {
         SceneManager.LoadScene(gameSceneName);
+    }
+    
+    public static void LoadCreateOwnScene()
+    {
+        SceneManager.LoadScene(createOwnSceneName);
     }
 }
