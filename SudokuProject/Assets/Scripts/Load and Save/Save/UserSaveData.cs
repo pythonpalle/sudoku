0918b7ea064e88fae1934849a3f4c2fda9aad2db
pythonpalle <patrikwlindfors@gmail.com>
2023-12-11 09:03:31 +0100

    före binary convert

--- SudokuProject/Assets/Scripts/Load and Save/Save/UserSaveData.cs
+++ SudokuProject/Assets/Scripts/Load and Save/Save/UserSaveData.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
 
 namespace Saving
 {
     [System.Serializable]
-    public class UserSaveData : SaveData
+    public class UserSaveData //: SaveData
     {
         public UserSaveData(UserIdentifier identifier)
         {
@@ -18,6 +21,53 @@ namespace Saving
 
         public UserIdentifier identifier;// { get; private set; }
         public List<PuzzleDataHolder> puzzles;// { get; private set; }
+        
+        /// <summary>
+        /// Converts the public fields of this game object to a JSON representation and returns it as a string.
+        /// </summary>
+        public string ToJson()
+        {
+            //return JsonUtility.ToJson(this);
+            return JsonUtility.ToJson(this, false);
+        }
+    
+        /// <summary>
+        /// Overrides the public fields in this object from the JSON string representation.
+        /// </summary>
+        public void LoadFromJson(string jsonString)
+        {
+            JsonUtility.FromJsonOverwrite(jsonString, this);
+        }
+        
+        public byte[] ToBinary()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            MemoryStream memoryStream = new MemoryStream();
+            
+            formatter.Serialize(memoryStream, this); 
+
+            // Get the serialized data as a byte array
+            byte[] serializedData = memoryStream.ToArray();
+            return serializedData;
+        }
+    
+        public void LoadFromBinary()
+        {
+            byte[] bytes = FileManager.ReadAllBytes("byteTest");
+        
+            BinaryFormatter formatter = new BinaryFormatter();
+            MemoryStream memoryStream = new MemoryStream(bytes);
+        
+            // deserializing
+            UserSaveData deserializedObject = (UserSaveData)formatter.Deserialize(memoryStream);
+            LoadFromDeserialized(deserializedObject);
+        }
+
+        private void LoadFromDeserialized(UserSaveData deserialized)
+        {
+            puzzles = deserialized.puzzles;
+            identifier = deserialized.identifier;
+        }
     }
 }
 
