17e1e7a958c35b700a7bac0419132be107732b8f
pythonpalle <patrikwlindfors@gmail.com>
2023-12-16 14:34:13 +0100

    innan tilebehaviour update

--- SudokuProject/Assets/TileColorFiller.cs
+++ SudokuProject/Assets/TileColorFiller.cs
@@ -11,39 +11,39 @@ public class TileColorFiller : MonoBehaviour
     [SerializeField] ColorObject baseColor;
 
     private Texture2D texture; // The texture representing the color wheel
-    private Color[] pixelsBeforeRed;
-    public List<int> currentColorNumbers = new List<int>();
-    public List<int> testColors = new List<int>();
+    //private Color[] pixelsBeforeRed;
+    // public List<int> currentColorNumbers = new List<int>();
+    // public List<int> testColors = new List<int>();
     
-    public bool isContradicted;
+    //public bool isContradicted;
 
     public ImageMerger merger;
 
-    private void RemoveAllColors()
-    {
-        currentColorNumbers.Clear();
-    }
-    
-    private void AddOrRemoveColor(int number)
-    {
-        if (currentColorNumbers.Contains(number))
-            currentColorNumbers.Remove(number);
-        else
-            currentColorNumbers.Add(number);
-    }
+    // private void RemoveAllColors()
+    // {
+    //     currentColorNumbers.Clear();
+    // }
+    //
+    // private void AddOrRemoveColor(int number)
+    // {
+    //     if (currentColorNumbers.Contains(number))
+    //         currentColorNumbers.Remove(number);
+    //     else
+    //         currentColorNumbers.Add(number);
+    // }
     
 
     // Method to split the texture into sections based on the number of colors added (like a color wheel)
-    public void SetTileColors()
+    public void SetTileColors(List<int> colorMarks, bool contradicted)
     {
-        int colorCount = currentColorNumbers.Count;
+        int colorCount = colorMarks.Count;
         if (colorCount <= 1)
         {
-            HandleOneColorFill(colorCount);
+            HandleOneColorFill(colorMarks, colorCount, contradicted);
         }
         else
         {
-            currentColorNumbers.Sort();
+            //currentColorNumbers.Sort();
 
             float deltaFill = 1.0f / colorCount;
             float sectionFill = deltaFill;
@@ -51,7 +51,7 @@ public class TileColorFiller : MonoBehaviour
             for (int i = 0; i < colorCount; i++)
             {
                 sections[i].gameObject.SetActive(true);
-                var color = GetColorForSection(i);
+                var color = GetColorForSection(colorMarks, i, contradicted);
 
                 
                 
@@ -68,18 +68,18 @@ public class TileColorFiller : MonoBehaviour
 
     }
 
-    private List<FillTileSection> GetActiveSections()
-    {
-        List<FillTileSection> activeSections = new List<FillTileSection>();
-        for (int i = 0; i < currentColorNumbers.Count; i++)
-        {
-            activeSections.Add(sections[i]);
-        }
+    // private List<FillTileSection> GetActiveSections()
+    // {
+    //     List<FillTileSection> activeSections = new List<FillTileSection>();
+    //     for (int i = 0; i < currentColorNumbers.Count; i++)
+    //     {
+    //         activeSections.Add(sections[i]);
+    //     }
+    //
+    //     return activeSections;
+    // }
 
-        return activeSections;
-    }
-
-    private void HandleOneColorFill(int colorCount)
+    private void HandleOneColorFill(List<int> colorNumbers, int colorCount, bool isContradicted)
     {
         if (colorCount == 0)
         {
@@ -88,13 +88,13 @@ public class TileColorFiller : MonoBehaviour
         }
         else if (colorCount == 1)
         {
-            Color color = GetColorForSection(0);
+            Color color = GetColorForSection(colorNumbers, 0, isContradicted);
             sections[0].SetFill(color, 1f);
         }
     }
 
 
-    private Color GetColorForSection(int sectionIndex)
+    private Color GetColorForSection(List<int> currentColorNumbers, int sectionIndex, bool isContradicted)
     {
         int tileIndex = currentColorNumbers[sectionIndex] - 1;
 
@@ -106,59 +106,59 @@ public class TileColorFiller : MonoBehaviour
         return color;
     }
 
-    private void RemoveContradiction()
-    {
-        if (!isContradicted) return;
+    // private void RemoveContradiction()
+    // {
+    //     if (!isContradicted) return;
+    //
+    //     isContradicted = false;
+    // }
+    //
+    // private void SetContradiction()
+    // {
+    //     if (isContradicted) return;
+    //
+    //     isContradicted = true;
+    // }
 
-        isContradicted = false;
-    }
-
-    private void SetContradiction()
-    {
-        if (isContradicted) return;
-
-        isContradicted = true;
-    }
-
-    private void Update() 
-    {
-        for (int number = 1; number <= 9; number++)
-        {
-            if (InputManager.NumberKeyDown(number))
-            {
-                AddOrRemoveColor(number);
-                SetTileColors();
-            }
-        }
-
-        if (InputManager.RemoveButtonIsPressed)
-        {
-            RemoveAllColors();
-            SetTileColors();
-        }
-
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            SetContradiction();
-            SetTileColors();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            RemoveContradiction();
-            SetTileColors();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            currentColorNumbers = new List<int>(testColors);
-            SetTileColors();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            SetTileColors();
-            merger.Merge(GetActiveSections());
-        }
-    }
+    // private void Update() 
+    // {
+    //     for (int number = 1; number <= 9; number++)
+    //     {
+    //         if (InputManager.NumberKeyDown(number))
+    //         {
+    //             AddOrRemoveColor(number);
+    //             SetTileColors();
+    //         }
+    //     }
+    //
+    //     if (InputManager.RemoveButtonIsPressed)
+    //     {
+    //         RemoveAllColors();
+    //         SetTileColors();
+    //     }
+    //
+    //     if (Input.GetKeyDown(KeyCode.C))
+    //     {
+    //         SetContradiction();
+    //         SetTileColors();
+    //     }
+    //     
+    //     if (Input.GetKeyDown(KeyCode.R))
+    //     {
+    //         RemoveContradiction();
+    //         SetTileColors();
+    //     }
+    //     
+    //     if (Input.GetKeyDown(KeyCode.T))
+    //     {
+    //         currentColorNumbers = new List<int>(testColors);
+    //         SetTileColors();
+    //     }
+    //     
+    //     if (Input.GetKeyDown(KeyCode.M))
+    //     {
+    //         SetTileColors();
+    //         merger.Merge(GetActiveSections());
+    //     }
+    // }
 }
