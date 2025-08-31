3acd654ab570f2c2f381a94e48909c970d0f6e99
pythonpalle <patrikwlindfors@gmail.com>
2025-08-31 17:13:35 +0200

    last changes before new unity version

---
+++ SudokuProject/Assets/Scripts/UI/UI Toolkit/LocalizedButton.cs
@@ -0,0 +1,20 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UIElements;
+// using UnityEditor.UIElements;
+//
+// //using UnityEngine.UI;
+//
+// [UxmlElement]
+// public partial class LocalizedButton : Button
+// {
+//     public static BindingId keyProperty = nameof(key);
+//
+//     [UxmlAttribute] public string key;
+//     
+//     public LocalizedButton()
+//     {
+//         
+//     }
+// } 
