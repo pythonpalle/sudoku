8d9251d79bd4c87b2936db97010042a35e80ad47
pythonpalle <patrikwlindfors@gmail.com>
2026-05-17 16:54:26 +0200

    Removed unused New Puzzle Popup

--- SudokuProject/Assets/Scripts/Scriptable Objects/PopupDataObject.cs
+++ SudokuProject/Assets/Scripts/Scriptable Objects/PopupDataObject.cs
@@ -1,39 +1,39 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-
-[CreateAssetMenu(fileName = "PopupDataObject", menuName = "Scriptable Objects/PopupDataObject")]
-public class PopupDataObject : ScriptableObject
-{
-    [Header("Title")]
-    public string Title;
-    
-    [Header("Size")]
-    [SerializeField] WindowSize height = WindowSize.Medium;
-    [SerializeField] WindowSize width = WindowSize.Medium;
-    
-    public int Height => _sizeLookup[height];
-    public int Width => _sizeLookup[width];
-
-    [Header("Content")]
-    public PopupContentsBehaviour PopupContent;
-    [SerializeField] private bool contentCoversFooterArea = false;
-
-    [Header("Buttons")]
-    public List<ButtonData> ButtonDatas;
-
-    [Header("Confirm Popup Data")] 
-    public string ExplanationText;
-    public bool UseCancelButton;
-    public string ConfirmButtonText;
-    
-    private Dictionary<WindowSize, int> _sizeLookup = new Dictionary<WindowSize, int>()
-    {
-        { WindowSize.Small, 450 },
-        { WindowSize.Medium, 900 },
-        { WindowSize.Large, 1350 }
-    };
-    
-
-}
+// using System;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.Events;
+//
+// [CreateAssetMenu(fileName = "PopupDataObject", menuName = "Scriptable Objects/PopupDataObject")]
+// public class PopupDataObject : ScriptableObject
+// {
+//     [Header("Title")]
+//     public string Title;
+//     
+//     [Header("Size")]
+//     [SerializeField] WindowSize height = WindowSize.Medium;
+//     [SerializeField] WindowSize width = WindowSize.Medium;
+//     
+//     public int Height => _sizeLookup[height];
+//     public int Width => _sizeLookup[width];
+//
+//     [Header("Content")]
+//     public PopupContentsBehaviour PopupContent;
+//     [SerializeField] private bool contentCoversFooterArea = false;
+//
+//     [Header("Buttons")]
+//     public List<ButtonData> ButtonDatas;
+//
+//     [Header("Confirm Popup Data")] 
+//     public string ExplanationText;
+//     public bool UseCancelButton;
+//     public string ConfirmButtonText;
+//     
+//     private Dictionary<WindowSize, int> _sizeLookup = new Dictionary<WindowSize, int>()
+//     {
+//         { WindowSize.Small, 450 },
+//         { WindowSize.Medium, 900 },
+//         { WindowSize.Large, 1350 }
+//     };
+//     
+//
+// }
